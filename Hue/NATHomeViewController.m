8dc322f568153a20e049f995a662dbb5f1755843
shakked <zacharyshakked@Zacharys-MacBook-Pro.local>
2014-12-13 22:55:24 -0500

    Pose Changes Recognized

--- Hue/NATHomeViewController.m
+++ Hue/NATHomeViewController.m
@@ -11,6 +11,8 @@
 
 @interface NATHomeViewController ()
 
+@property (strong, nonatomic) TLMPose *currentPose;
+
 - (IBAction)connectMyo:(id)sender;
 @end
 
@@ -28,6 +30,12 @@
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(didReceiveOrientationEvent:)
                                                  name:TLMMyoDidReceiveOrientationEventNotification
+                                               object:nil];
+    
+    // Posted when a new pose is available from a TLMMyo.
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(didReceivePoseChange:)
+                                                 name:TLMMyoDidReceivePoseChangedNotification
                                                object:nil];
 }
 
@@ -53,17 +61,53 @@
     [self.navigationController pushViewController:settings animated:YES];
 }
 
-- (void)didReceivePoseChange:(NSNotification*)notification {
-    TLMPose *pose = notification.userInfo[kTLMKeyUnlockEvent];
-    
-    //TODO: do something with the pose object.
-    NSLog(@"this is a pose: %@", pose);
-}
 
 - (void)didReceiveOrientationEvent:(NSNotification*)notification {
     TLMOrientationEvent *orientation = notification.userInfo[kTLMKeyOrientationEvent];
     
     //TODO: do something with the orientation object.
-     NSLog(@"this is an orientation event");
+    
+}
+- (void)didReceivePoseChange:(NSNotification *)notification {
+    // Retrieve the pose from the NSNotification's userInfo with the kTLMKeyPose key.
+    TLMPose *pose = notification.userInfo[kTLMKeyPose];
+    self.currentPose = pose;
+    // Handle the cases of the TLMPoseType enumeration, and change the color of helloLabel based on the pose we receive.
+    switch (pose.type) {
+        case TLMPoseTypeUnknown:
+        case TLMPoseTypeRest:
+        case TLMPoseTypeDoubleTap:
+            // Changes helloLabel's font to Helvetica Neue when the user is in a rest or unknown pose.
+            NSLog(@"Unknown, rest, double tap");
+            break;
+        case TLMPoseTypeFist:
+            // Changes helloLabel's font to Noteworthy when the user is in a fist pose.
+            NSLog(@"fist!");
+            break;
+        case TLMPoseTypeWaveIn:
+            // Changes helloLabel's font to Courier New when the user is in a wave in pose.
+            NSLog(@"wave in pose");
+            break;
+        case TLMPoseTypeWaveOut:
+            // Changes helloLabel's font to Snell Roundhand when the user is in a wave out pose.
+            NSLog(@"Snell roundhand");
+            break;
+        case TLMPoseTypeFingersSpread:
+            // Changes helloLabel's font to Chalkduster when the user is in a fingers spread pose.
+            NSLog(@"fingers spread");
+            break;
+    }
+    // Unlock the Myo whenever we receive a pose
+    if (pose.type == TLMPoseTypeUnknown || pose.type == TLMPoseTypeRest) {
+        // Causes the Myo to lock after a short period.
+        [pose.myo unlockWithType:TLMUnlockTypeTimed];
+    } else {
+        // Keeps the Myo unlocked until specified.
+        // This is required to keep Myo unlocked while holding a pose, but if a pose is not being held, use
+        // TLMUnlockTypeTimed to restart the timer.
+        [pose.myo unlockWithType:TLMUnlockTypeHold];
+        // Indicates that a user action has been performed.
+        [pose.myo indicateUserAction];
+    }
 }
 @end
